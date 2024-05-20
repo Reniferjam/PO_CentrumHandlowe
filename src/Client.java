172dfb426a6f88afd0783388c27388dea7d88be1
Mateusz Wasilewski <matiw2468@gmail.com>
2024-05-20 18:11:06 +0200

    Dodano zmienne i funkcje do klasy Client

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,25 @@
-public class Client {
+public class Client
+{
+    private  Item[] shopList;
+    private  int currentShopID, currentProductID;
+
+    private boolean checkProduct()
+    {
+        return true;
+    }
+
+    private void buyProduct()
+    {
+
+    }
+
+    public int getCurrentProductID()
+    {
+        return currentProductID;
+    }
+
+    public int getCurrentShopID()
+    {
+        return currentShopID;
+    }
 }
