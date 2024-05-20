cd6392b01847ce3670c5c23ee04435259bb6094d
Mateusz Wasilewski <matiw2468@gmail.com>
2024-05-20 18:03:20 +0200

    Dodano zmienne i funkcje klasy Item

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,40 @@
-public class Item {
+public class Item
+{
+    private String itemName;
+    private int itemID, itemQuantity, itemQuality;
+    private double itemPrice;
+
+    private Item(String itemName, int itemID, int itemQuantity, int itemQuality, int itemPrice)
+    {
+        this.itemName = itemName;
+        this.itemID = itemID;
+        this.itemQuantity = itemQuantity;
+        this.itemQuality = itemQuality;
+        this.itemPrice = itemPrice;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public int getItemQuality() {
+        return itemQuality;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
 }
