c72babe8ed36b9f106f0f5be50cd2c5a108f38e2
Jan Gromiec <jasiu05gromiec@gmail.com>
2024-05-21 10:53:19 +0200

    Poczatek do randoma

--- src/Random.java
+++ src/Random.java
@@ -1,2 +1,14 @@
-public class Random {
+public class Random extends Client
+{
+    public int randomShopID;
+
+    private int findRandomShop()
+    {
+        return 0;
+    }
+
+    private int getRandomShopID()
+    {
+        return 0;
+    }
 }
